dda1d38775bf6ab48205b7c315e8aa18063ef8ab
RameesAP <rameeswork@gmail.com>
2023-04-01 03:02:53 +0530

    left completed

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -15,9 +15,37 @@ const Hero = () => {
                 {/* Hero heading */}
                 <div className="hero-text">
                     <div>
-                        <span>Shape</span>
+                        <span className='stroke-text'>Shape </span>
                         <span>Your</span>
                     </div>
+                    <div>
+                        <span>Ideal body</span>
+                    </div>
+                    <div>
+                        <span>In here we will help you to shape and build your ideal body and live up your life to fullest</span>
+                    </div>
+                </div>
+
+                {/* figuers */}
+
+                <div className="figures">
+                    <div>
+                        <span>+140</span>
+                        <span>expert coaches</span>
+                    </div>
+                    <div>
+                        <span>+978</span>
+                        <span>members joined</span>
+                    </div>
+                    <div>
+                        <span>+50</span>
+                        <span>fitness programs</span>
+                    </div>
+                </div>
+                {/* hero buttons */}
+                <div className="hero-buttons">
+                    <button className="btn">Get Started</button>
+                    <button className="btn">Learn More</button>
                 </div>
             </div>
             <div className="right-h">right side</div>
