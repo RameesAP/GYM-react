840572086d07a4f1522f0cb2631e5e8217ebb7d0
RameesAP <rameeswork@gmail.com>
2023-04-04 03:43:37 +0530

    test moni completed

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Hero from './components/Hero/Hero';
 import Programs from './components/Programs/Programs';
 import Reasons from './components/Reasons/Reasons';
 import Plans from './components/Plans/Plans';
+import Testimonials from './components/Testimonials/Testimonials';
+import Join from './components/Join/Join';
 function App() {
   return (
     <div className="App">
@@ -10,6 +12,8 @@ function App() {
           <Programs />
           <Reasons />
           <Plans />
+          <Testimonials />
+          <Join />
     </div>
   );
 }
