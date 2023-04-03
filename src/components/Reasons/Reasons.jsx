5771c4e635a3ad8af489116393941430278f2725
RameesAP <rameeswork@gmail.com>
2023-04-04 01:10:27 +0530

    Resons complted

--- src/components/Reasons/Reasons.jsx
+++ src/components/Reasons/Reasons.jsx
@@ -27,7 +27,27 @@ const Reasons = () => {
           <span> Choose us</span>
         </div>
 
-        
+        <div className='details-r'>
+          <div><img src={tick} alt=""></img>
+            <span>OVER 140+ EXPERT COACHS</span>
+          </div>
+          <div><img src={tick} alt="" />
+            <span>TRAIN SMARTER AND FASTER THAN BEFORE</span>
+          </div>
+          <div><img src={tick} alt="" />
+            <span>1 FREE PROGRAM FOR NEW MEMBER</span>
+          </div>
+          <div><img src={tick} alt="" />
+            <span>RELIABLE PARTNERS</span>
+          </div>
+        </div>
+        <span style={{ color: "var(--gray)", fontWeight: "normal" }}>OUR PARTNERS</span>
+
+        <div className="partners">
+          <img src={nb} alt="" />
+          <img src={adidas} alt="" />
+          <img src={nike} alt="" />
+        </div>
       </div>
     </div>
   )
