7beca3804c3bf4141cd229b981460104e2507c08
RameesAP <rameeswork@gmail.com>
2023-04-03 23:25:14 +0530

    image

--- src/components/Reasons/Reasons.jsx
+++ src/components/Reasons/Reasons.jsx
@@ -13,7 +13,10 @@ const Reasons = () => {
   return (
    <div className="Reasons" id='reasons'>
     <div className="left-r">
-      <img src="" alt="" /><img src="" alt="" /><img src="" alt="" /><img src="" alt="" />
+      <img src={image1} alt="" />
+      <img src={image2} alt="" />
+      <img src={image3} alt="" />
+      <img src={image4} alt="" />
 
     </div>
     <div className="right-r">
